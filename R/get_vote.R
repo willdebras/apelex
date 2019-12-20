32b95dd6479bcc1a936956a561d0570d10172eac
Will Bonnell <wtb3na@virginia.edu>
2019-12-20 10:49:23 -0600

    added more optional binary parameters

--- R/get_vote.R
+++ R/get_vote.R
@@ -27,7 +27,11 @@ get_vote <- function(key = Sys.getenv("apelex_api_key"),
                      level = NULL,
                      office_id = NULL,
                      winner = NULL,
-                     race_type = NULL) {
+                     race_type = NULL,
+                     party = NULL,
+                     test = FALSE,
+                     national = TRUE,
+                     uncontested = FALSE) {
 
 
   base_url <- "https://api.ap.org/v2/"
@@ -35,6 +39,7 @@ get_vote <- function(key = Sys.getenv("apelex_api_key"),
   end_point <- "elections/"
 
   url_full <- paste0(base_url, end_point, date)
+
 
   raw_apelex <- httr::GET(url_full, query = list(
 
@@ -45,11 +50,16 @@ get_vote <- function(key = Sys.getenv("apelex_api_key"),
     officeID = office_id,
     winner = winner,
     raceTypeId = race_type,
-    test = "true"
+    test = test,
+    party = party,
+    national = national,
+    uncontested = uncontested
 
     )
 
   )
+
+
 
   parsed_apelex <- jsonlite::fromJSON(
     httr::content(raw_apelex, "text"),
