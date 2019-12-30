e722290c468cbb266a1f15d00dab1b5ffc0b83d9
Will Bonnell <wtb3na@virginia.edu>
2019-12-30 15:45:08 -0600

    adding shell for get_vat() function

---
+++ R/get_vat.R
@@ -0,0 +1,23 @@
+#' get_report
+#'
+#' @param key
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_vat <- function(key = Sys.getenv("apelex_api_key"),
+                       ) {
+
+
+
+
+
+
+
+
+
+
+
+
+}
