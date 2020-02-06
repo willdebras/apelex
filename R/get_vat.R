25a67f837d9f22ca95a6154416f0c64fc9a26eff
Will Bonnell <wtb3na@virginia.edu>
2020-02-06 12:01:14 -0600

    committing updated VAT function to lookup the appropriate report and query from it

--- R/get_vat.R
+++ R/get_vat.R
@@ -5,14 +5,17 @@
 #' @param key Supplied API key
 #' @param test TRUE/FALSE. Whether to use test data (TRUE) or production data (FALSE). Defaults to FALSE.
 #' @param report The type of VAT report to get data from. Options include: CountyGeoStrata, CountyPartyStrataEstimates, CountyGeoStrataEstimates, CountyPartyStatewideEstimates, CountyGeoStatewideEstimates
+#' @param date date of the election in ISO format, e.g. "2020-02-03". Defaults to Sys.Date()
 #'
 #' @return Returns a tidy dataframe of VAT results
 #' @export
 #'
 #' @importFrom httr GET http_type content
 #' @importFrom jsonlite fromJSON
+#' @importFrom dplyr filter
+#' @importFrom tidyr unnest
 #'
-#' @examples vat_test <- get_vat(report = "CountyGeoStrata", key = "key = Sys.getenv("apelex_api_key"))
+#' @examples vat_test <- get_vat(report = "CountyGeoStrata", key = "key = Sys.getenv("apelex_api_key"), date = "2020-02-03)
 
 get_vat <- function(key = Sys.getenv("apelex_api_key"),
                     test = FALSE,
@@ -21,34 +24,47 @@ get_vat <- function(key = Sys.getenv("apelex_api_key"),
                                "CountyPartyStrataEstimates",
                                "CountyGeoStrataEstimates",
                                "CountyPartyStatewideEstimates",
-                               "CountyGeoStatewideEstimates")) {
+                               "CountyGeoStatewideEstimates"),
+                    date = Sys.Date()) {
 
-report <- switch(report,
-                   CountyGeoStrata = "9ce6742096324717bada64791239a351",
-                   CountyPartyStrata = "360f8d5addc34ca6b45e7a3d2745bd97",
-                   CountyPartyStrataEstimates = "97bf0dbf9fd74784a7b76fbf8c161fde",
-                   CountyGeoStrataEstimates = "2ed5c8076dda4bf6ad19d81df68efa40",
-                   CountyPartyStatewideEstimates = "a31b5d1e96624d679419b23775f3467d",
-                   CountyGeoStatewideEstimates = "fad384546bf145b3b7ece2f31f629bb8")
+  url <- "https://api.ap.org/v2/reports?type=SPE_VATEstimate"
 
-base_url <- "https://api.ap.org/v2/"
+  vat_reps  <- httr::GET(url, query = list(
 
-end_point <- "reports/"
+    apikey = key,
+    format = "json"
 
-url_full <- paste0(base_url, end_point, report)
-
-raw_apvat <- httr::GET(url_full, query = list(
-
-  apikey = key
+    )
 
   )
 
-)
+  parsed_vat_reps <- jsonlite::fromJSON(httr::content(vat_reps, "text"), simplifyDataFrame = TRUE)
 
-parsed_vat <- jsonlite::fromJSON(httr::content(raw_apvat, "text"), simplifyDataFrame = TRUE)
+  urls_df <- tidyr::unnest(parsed_vat_reps$reports, cols = c(categories))[c(FALSE, TRUE, FALSE, FALSE),]
 
-parsed_vat_df <- parsed_vat[[1]]
+  urls_date <- dplyr::filter(urls_df, electionDate == !!date)
 
-return(parsed_vat_df)
+
+  urls_switch <- paste0(urls_date$term, " = ", "\"", urls_date$id, "\"", collapse = ",")
+
+  switch <- paste0("switch(", "\"", report, "\"", ",", urls_switch, ")")
+
+  report_new <- eval(parse(text = switch))
+
+
+  raw_apvat <- httr::GET(report_new, query = list(
+
+    apikey = key
+
+    )
+
+  )
+
+  parsed_vat <- jsonlite::fromJSON(httr::content(raw_apvat, "text"), simplifyDataFrame = TRUE)
+
+  parsed_vat_df <- parsed_vat[[1]]
+
+  return(parsed_vat_df)
 
 }
+
